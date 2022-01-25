61b27a632213bcb131ad990fc307f491a6fc4e18
Colin <colin.obrien590@gmail.com>
2022-01-25 16:19:25 -0500

    solved first 3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
 const reverseString = function(str) {
+    let letters = str.split("")
+    const len = str.length
+    let reversed = ''
 
+    for (let i=0; i<len; i++) {
+        reversed += letters.pop();
+    }
+    return reversed
 };
 
 // Do not edit below this line
