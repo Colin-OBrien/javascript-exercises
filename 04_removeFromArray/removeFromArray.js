b83e5b910988b75fff3622590ae5af3a250f3ff9
Colin <colin.obrien590@gmail.com>
2022-01-26 14:33:01 -0500

    completed exercise 4 and working on 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value) {
+    let args = Array.from(arguments)
+    let forDeletion = args.splice(1, arr.length)
+  
+    arr = arr.filter(item=> !forDeletion.includes(item))
+    console.log(arr)
 
+    return arr
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let totalSum = 0
 
+
+    if (start <= end) {
+        for (let begin = start; start <= end; begin++) {
+
+            totalSum += start
+
+        }
+    } 
+    /*
+    else if (start >= end) {
+        for (start; start >= end; start--) {
+
+            totalSum += start
+
+    }
+    return totalSum
+}*/
+return totalSum
 };
 
 // Do not edit below this line
